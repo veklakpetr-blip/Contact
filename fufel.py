4ead2a1e46d68d92cc904cce103c3946518cde2a
UnbracedFrog <unbracedfrog@gmail.com>
2026-06-22 15:02:42 +0300

    Смоделировал машину

--- fufel.py
+++ fufel.py
@@ -1,3 +1,27 @@
-bbrr = 111
-ggg = 66
-print(bbrr + ggg)
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+app = Ursina()
+
+player = FirstPersonController(
+    position=(0, 0, 0),
+    speed=2,
+    model='box'
+)
+
+# Создаем пол
+ground = Entity(
+    model='plane',
+    scale=(1000, 1, 1000),
+    color=color.green,
+    texture='white_cube',
+    collider='box'
+)
+
+Sky()
+
+def input(key):
+    if key == "escape":
+        quit()
+
+app.run()
