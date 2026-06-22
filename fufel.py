3adf88c53a39d82afd8ec342149dbc8b150c88bf
BetaPetruska228 <veklakpetr@gmail.com>
2026-06-22 19:57:47 +0300

    добавление контента автомобилю

--- fufel.py
+++ fufel.py
@@ -2,17 +2,19 @@ from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina.shaders import lit_with_shadows_shader
 
+suprax = 100
+
 app = Ursina()
 
 player = FirstPersonController(
-    position=(0, 0, 0),
+    position=(0, 0, -10),
     speed=7,
     model='box'
 )
 
 supra = Entity(
     model='assets/car.obj',
-    position=(100, 0.3, 0),
+    position=(suprax, 0.3, 0),
     rotation=(0, 180, 0),
     color=color.black,
     collider='mesh',
@@ -34,5 +36,14 @@ sun.look_at(Vec3(1, -1, -1))
 def input(key):
     if key == "escape":
         quit()
+
+def zadacha1():
+    global suprax
+
+    if supra.position[0] != 0:
+        supra.position -= Vec3(0.5, 0, 0)
+
+def update():
+    zadacha1()
 
 app.run()
