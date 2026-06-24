f06c8b0356b9bd84c143a24baf1accb7d2669184
UnbracedFrog <unbracedfrog@gmail.com>
2026-06-24 08:36:57 +0300

    Сделал вождение машины

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from ursina import *
-from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina.shaders import lit_with_shadows_shader
 
-from modules.constants import PLAYER_SPEED, PLAYER_FOV, SUPRA_BORDER, SUPRA_STOP_SCALE
+from modules.controller import Controller
+from modules.constants import PLAYER_SPEED, PLAYER_NORMAL_HEIGHT, PLAYER_DRIVE_HEIGHT, CAMERA_FOV, SUPRA_BORDER, SUPRA_STOP_SCALE
 
 supra_x = -34
 supra_y = 0
 supra_speed = 0.5
 
+in_supra = False
+
 app = Ursina()
 
-player = FirstPersonController(
+player = Controller(
     position=(-25, 0, -10),
     rotation=(0, 270, 0),
+    height=PLAYER_NORMAL_HEIGHT,
     speed=PLAYER_SPEED
 )
 
@@ -44,21 +47,35 @@ Sky()
 
 sun = DirectionalLight()
 sun.look_at(Vec3(1, -1, -1))
-camera.fov = PLAYER_FOV
+camera.fov = CAMERA_FOV
 
 def input(key):
     if key == 'escape':
         quit()
-    if held_keys['right mouse']:
-        print('ddd')
+
+    if key == 'left mouse down':
+        camera_position = player.position + Vec3(0, player.height, 0)
+        ray = raycast(camera_position, camera.forward, PLAYER_NORMAL_HEIGHT)
+        if ray.entity == supra:
+            get_in_car()
 
 def zadacha1_1():
-    player.enabled = True
     global supra_x, supra_y, supra_speed
     supra.position -= Vec3(0, 0, supra_speed)
 
     if supra.position[2] < SUPRA_BORDER:
         supra_speed *= SUPRA_STOP_SCALE
+
+def get_in_car():
+    '''Садится в машину'''
+    global in_supra
+    if not in_supra:
+        supra.parent = player
+        supra.position = (0, 0, 0)
+        supra.rotation = (0, -90, 0)
+
+        player.set_height(PLAYER_DRIVE_HEIGHT)
+        in_supra = True
 
 def update():
     zadacha1_1()
